c7de718156e8939fa4c17cdd1b70d21cda927c30
Baxtiyor O'ktamov <155018356+James21332d@users.noreply.github.com>
2026-02-11 20:22:52 +0500

    Update 0002-add-two-numbers.go

--- 0002-add-two-numbers/0002-add-two-numbers.go
+++ 0002-add-two-numbers/0002-add-two-numbers.go
@@ -1,10 +1,3 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
     dummy := &ListNode{}
     curr := dummy
